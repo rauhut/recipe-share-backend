3053b2b71aabc0cd10e8c6f478d9f961b225c4ed
rauhut <jackierauhut@gmail.com>
2020-07-20 14:38:13 -0400

    update server.js to connect to production db

--- db/index.js
+++ db/index.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/recipe-share", { useNewUrlParser: true })
+  .connect(
+    process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/recipe-share",
+    { useNewUrlParser: true }
+  )
   .catch((e) => {
     console.error("Connection error", e.message);
   });
